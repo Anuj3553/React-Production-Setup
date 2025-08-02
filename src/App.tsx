474c738c74e5a88e25f8046b437c1f4575513f33
Anuj3553 <anujverma3553@gmail.com>
2025-08-02 17:03:35 +0530

    feat: sentry setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Feature1Page } from '@features/feature1'
 
 function App() {
+    throw new Error('This is a test error for Sentry!')
     return (
         <div className="bg-amber-300">
             React Production Setup
-            <Feature1Page></Feature1Page>
+            <Feature1Page />
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import '@shared/assets/styles/index.css'
 import App from './App.tsx'
 
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+    dsn: 'https://ab398538822638b1c526bec4f3d0513a@o4509773786578944.ingest.us.sentry.io/4509774183727104',
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    enabled: import.meta.env.VITE_ENV === 'production',
+    sendDefaultPii: true
+})
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
